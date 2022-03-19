0a7bb2761f3c69f94f11e9ddd9b07be92912f5ef
r3tr0w0lf <69786157+r3tr0w0lf@users.noreply.github.com>
2022-03-20 02:03:09 +0530

    timed builds

--- build.sh
+++ build.sh
@@ -31,12 +31,12 @@ kernel_prepare () {
 }
 
 kernel_build () {
-    make -j$(nproc) &>/dev/null
+    time make -j$(nproc) &>/dev/null
     verbosity "KERNEL BUILD COMPLETED SUCCESSFULLY"
 }
 
 kernel_package () {
-    7z a -t7z $workdir/linux.7z /usr/src/linux-* &>/dev/null
+    time 7z a -t7z $workdir/linux.7z /usr/src/linux-* &>/dev/null
     verbosity "KERNEL PACKGING COMPLETED SUCCESSFULLY"
 }
 
