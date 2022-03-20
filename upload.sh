ef96c65e982620555ba252075667ec5269cc6860
r3tr0w0lf <69786157+r3tr0w0lf@users.noreply.github.com>
2022-03-20 13:06:25 +0530

    upload: add release tag logic

--- upload.sh
+++ upload.sh
@@ -17,6 +17,7 @@ verbosity () {
 
 kernel_release () {
     echo $GITHUB_CLI_TOKEN > github_cli_token
+    release_tag=$(cat $workdir/release_tag)
     gh auth login --with-token < github_cli_token
     gh release create $release_tag $workdir/linux.7z --generate-notes
 }
