6cce9a98654b2686bd2c68a0ad088b28ef91527f
r3tr0w0lf <69786157+r3tr0w0lf@users.noreply.github.com>
2022-03-20 12:36:45 +0530

    upload: remove temp upload

--- upload.sh
+++ upload.sh
@@ -5,7 +5,7 @@ trap "exit" INT
 set -e
 
 # Author: https://github.com/x0rzavi
-# Description: Release to github and temporarily upload built kernel archive to https://temp.sh/
+# Description: Release to github
 # Dependencies: github-cli
 
 workdir=$(pwd)
@@ -15,18 +15,10 @@ verbosity () {
     echo -e "***********************************************\n\n"
 }
 
-#kernel_upload () {
-#    release_tag=$(cat $workdir/release_tag)
-#    curl --no-progress-meter -T $workdir/linux.7z temp.sh > $workdir/download_link.txt
-#    download_link=$(cat $workdir/download_link.txt)
-#    verbosity $download_link
-#}
-
 kernel_release () {
     echo $GITHUB_CLI_TOKEN > github_cli_token
     gh auth login --with-token < github_cli_token
-    gh release create $release_tag $workdir/linux.7z $workdir/download_link.txt --generate-notes
+    gh release create $release_tag $workdir/linux.7z --generate-notes
 }
 
-#kernel_upload
 kernel_release
