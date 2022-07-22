41b71fc7fdb1fd1a2bb0add24b7b2aaaa28c824f
r3tr0w0lf <69786157+r3tr0w0lf@users.noreply.github.com>
2022-07-22 13:02:43 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -18,8 +18,8 @@ verbosity () {
 kernel_prepare () {
     cd /usr/src/linux
     cp CONFIGS/xanmod/gcc/config_x86-64-v3 .config
-    echo $PATH
-    time make -j$(nproc) olddefconfig
+    bash ls
+    time SHELL=/bin/bash make -j$(nproc) olddefconfig
     for patch in $workdir/patches/*
     do
         patch < $patch
